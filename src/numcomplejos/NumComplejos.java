e470cd9e7696f07eca2e37022827bc4d21c3734c
Gabriela <Aux457@gmail.com>
2019-09-01 03:04:45 -0400

    NumComplejos

---
+++ src/numcomplejos/NumComplejos.java
@@ -0,0 +1,11 @@
+
+package numcomplejos;
+
+public class NumComplejos {
+
+    public static void main(String[] args) {
+        Pantalla p = new Pantalla();
+        p.setVisible(true);
+    }
+    
+}
